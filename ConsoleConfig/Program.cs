d04ae53333fcb095bc799100b7e7cf5866cc7f45
lightcho <853358540@qq.com>
2018-08-14 10:56:12 +0800

    lianxi

--- ConsoleConfig/Program.cs
+++ ConsoleConfig/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
 namespace ConsoleConfig
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+		static void Main(string[] args)
+		{
+			var builder = new ConfigurationBuilder()
+					 .SetBasePath(Directory.GetCurrentDirectory())
+					 .AddJsonFile("appsettings.json")
+		             .AddJsonFile("appsettings.Test.json",true,reloadOnChange: true);
+			var config = builder.Build();
+			//foreach(var provider in config.Providers)
+			//{
+			//	provider.TryGet("Alipay:AppId",out string val);
+			//	Console.WriteLine(val);
+			//}
+			Console.WriteLine(config["Alipay:AppId"]);
+			Console.WriteLine(config["Alipay:PriviteKey"]);
+			Console.WriteLine("更改文件之后，按下任意键");
+			Console.ReadKey();
+			Console.WriteLine("change:");
+			//读取配置
+			Console.WriteLine(config["Alipay:AppId"]);
+			Console.WriteLine(config["Alipay:PriviteKey"]);
+			Console.ReadKey();
+		}
     }
 }
---
+++ ConsoleConfig/Readme.md
@@ -0,0 +1,3 @@
+﻿参考
+https://www.cnblogs.com/stulzq/p/8570496.html
+https://blog.csdn.net/only_yu_yy/article/details/78785403
