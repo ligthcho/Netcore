8128f65e6d9b92b9b9ba5f5214ac128de4c1ca2d
lightcho <853358540@qq.com>
2018-08-14 09:39:01 +0800

    添加项目文件。

---
+++ ConsoleConfig/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
